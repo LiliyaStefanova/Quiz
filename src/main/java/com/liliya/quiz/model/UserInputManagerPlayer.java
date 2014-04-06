5c47901c328b91c8547241c46e6528169653335f
Ejko <liliya.stefanova@gmail.com>
2014-04-06 20:45:36 +0100

    amended selectQuizToPlayFromList method

--- src/main/java/com/liliya/quiz/model/UserInputManagerPlayer.java
+++ src/main/java/com/liliya/quiz/model/UserInputManagerPlayer.java
@@ -9,38 +9,38 @@ import java.util.Scanner;
 
 public class UserInputManagerPlayer {
 
-    public int selectQuizToPlayFromList(){
+    public int selectQuizToPlayFromList() {
+        int choice = 0;
         System.out.print(">>");
         Scanner sc = new Scanner(System.in);
-        return sc.nextInt();
+        choice = sc.nextInt();
+        return choice;
     }
 
-    public int provideSelectedAnswer(){
-        int playerGuess=0;
-        do{
-            try{
+    public int provideSelectedAnswer() {
+        int playerGuess = 0;
+        do {
+            try {
                 System.out.print("Your answer(type the answer number): ");
                 Scanner sc = new Scanner(System.in);
                 playerGuess = sc.nextInt();
-                //TODO check if any of the answers provided are invalid
-
-            } catch(InputMismatchException ex){
+            } catch (InputMismatchException ex) {
                 System.out.println("Answer must be number of the question.Try again!");
             }
-        } while(!answerRangeValidationCheck(playerGuess));
+        } while (!answerRangeValidationCheck(playerGuess));
 
         return playerGuess;
     }
 
-    private boolean answerRangeValidationCheck(int answer){
-        boolean correct=true;
-        if(answer<1 || answer>4){
-            correct=false;
+    private boolean answerRangeValidationCheck(int answer) {
+        boolean correct = true;
+        if (answer < 1 || answer > 4) {
+            correct = false;
         }
         return correct;
     }
 
-    public String providePlayerName(){
+    public String providePlayerName() {
         System.out.print("Enter your name: ");
         Scanner sc = new Scanner(System.in);
         return sc.nextLine();
